5211eadb4a67544195396f47b185dc99d699553d
Faheem <faheemmbasheer@gmail.com>
2024-11-13 14:20:37 +0530

    set up connection to mongoDB

--- backEnd/src/app.ts
+++ backEnd/src/app.ts
@@ -1,9 +1,34 @@
 import Express from "express";
+import logger from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = Express();
-
 const PORT = 3000;
+
+app.use(Express.json());
+app.use(logger("dev"));
+app.use(cors());
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect("mongodb://localhost/UMS")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.log("Couldn't connect to MongoDB", err);
+  });
+
+// app.use();
+app.get("/api/users", (req, res, next) => {
+  res.json(users);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
+
+const users = [
+  { id: 1, name: "rifan" },
+  { id: 2, name: "thahir" },
+];
